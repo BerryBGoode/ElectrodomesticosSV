2cf203fd573cd9ca977fd8f659f59d8f12b921dd
BerryBGoode <fernandomena3131@gmail.com>
2023-05-22 12:06:03 -0600

    modificaciones página publica, redireciconar a página del articulo

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -18,24 +18,37 @@ document.addEventListener('DOMContentLoaded', async event => {
     const JSON = await request(PRODUCTOS, 'productos');
     if (JSON.status) {
         // TOAST.show();
-        console.log(JSON.data);
         let i = 0;
         JSON.data.forEach(element => {
             CONTAINER.innerHTML += `
             <div class="col-md-3">
                 <div class="card producto">
-                    <img src="${DIR+element.imagen}" class="card-img-top" alt="${element.nombre}">
+                    <img src="${DIR + element.imagen}" class="card-img-top" alt="${element.nombre}">
                     <div class="card-body">
                         <h5 class="card-title">${element.nombre}</h5>
                         <p class="card-text">${element.descripcion}.</p>
+                        <a class="btn btn-secondary ver" id="${element.idproducto}">Ver</a>
                         <a href="#" class="btn btn-secondary">Comprar</a>
+
                     </div>
                 </div>        
             </div>
-            `;        
+            `;
         });
-        // TOAST.container.show();
-    }else{
+        // evento para mostrar articulo
+        const VER = document.getElementsByClassName('ver');
+        // recorer los botones de la card
+        for(let i = 0; i < VER.length; i++){
+            VER[i].addEventListener('click', () => {
+                // encodeURIComponent valida dato valido para la URL
+                const URL = 'articulo.html'+ '?idproducto=' + encodeURIComponent(VER[i].id);
+                // console.log(VER[i].id);
+                // redireccionar a la página con el producto seleccionado
+                window.location.href = URL;
+            });
+        }
+    } else {
 
     }
 })
+
